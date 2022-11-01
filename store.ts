63255b4a947dcba398da8bdf11df448d00de407a
Oncryn <95271707+Oncryn@users.noreply.github.com>
2022-11-02 08:59:42 +1030

    Added 8 themes to the store

--- store.ts
+++ store.ts
@@ -4,7 +4,40 @@ interface Purchasable {
 }
 
 let store: { themes: Purchasable[] } = {
-    themes: []
+    themes: [
+        {
+            item: 1,
+            cost: 3000
+        },
+        {
+            item: 2,
+            cost: 8000
+        },
+        {
+            item: 3,
+            cost: 12000
+        },
+        {
+            item: 4,
+            cost: 30000
+        },
+        {
+            item: 8,
+            cost: 50000
+        },
+        {
+            item: 9,
+            cost: 5000
+        },
+        {
+            item: 10,
+            cost: 1000
+        },
+        {
+            item: 11,
+            cost: 80000
+        }
+    ]
 };
 
 export default store;
